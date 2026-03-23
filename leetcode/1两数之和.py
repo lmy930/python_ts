1cbca628e2540cf98aead12b10b1fa36e7325e47
lmy930 <mengyaoli0930@163..com>
2026-03-23 17:03:02 +0800

    add 两数之和

---
+++ leetcode/1两数之和.py
@@ -0,0 +1,172 @@
+'''
+给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那两个整数，并返回他们的数组下标。
+你可以假设每种输入只会对应一个答案。但是，数组中同一个元素不能使用两遍。
+
+示例:
+给定 nums = [2, 7, 11, 15], target = 9
+因为 nums[0] + nums[1] = 2 + 7 = 9
+所以返回 [0, 1]
+'''
+#暴力法
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        for i in range(len(nums)):
+            for j in range(i+1, len(nums)):
+                if nums[i] + nums[j] == target:
+                    return [i,j]
+
+
+
+#使用字典
+from typing import List
+
+from sklearn.base import defaultdict
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        records = dict()
+        
+        for index, value in enumerate(nums):  
+            if target - value in records:   # 遍历当前元素，并在map中寻找是否有匹配的key
+                return [records[target- value], index]
+            records[value] = index    # 如果没找到匹配对，就把访问过的元素和下标加入到map中
+        return []
+    
+#使用集合
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        #创建一个集合来存储我们目前看到的数字
+        seen = set()             
+        for i, num in enumerate(nums):
+            complement = target - num
+            if complement in seen:
+                return [nums.index(complement), i]
+            seen.add(num)
+        return []
+
+
+#使用双指针
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        # 对输入列表进行排序
+        nums_sorted = sorted(nums)
+        
+        # 使用双指针
+        left = 0
+        right = len(nums_sorted) - 1
+        while left < right:
+            current_sum = nums_sorted[left] + nums_sorted[right]
+            if current_sum == target:
+                # 如果和等于目标数，则返回两个数的下标
+                left_index = nums.index(nums_sorted[left])
+                right_index = nums.index(nums_sorted[right])
+                if left_index == right_index:
+                    right_index = nums[left_index+1:].index(nums_sorted[right]) + left_index + 1
+                return [left_index, right_index]
+            elif current_sum < target:
+                # 如果总和小于目标，则将左侧指针向右移动
+                left += 1
+            else:
+                # 如果总和大于目标值，则将右指针向左移动
+                right -= 1
+        return []
+    
+
+
+
+#242有效的字母异位词
+'''
+给定两个字符串 s 和 t ，编写一个函数来判断 t 是否是 s 的字母异位词。
+
+示例 1: 输入: s = "anagram", t = "nagaram" 输出: true
+
+示例 2: 输入: s = "rat", t = "car" 输出: false
+
+说明: 你可以假设字符串只包含小写字母。
+'''
+
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        record = [0] * 26
+        for i in s:
+            #并不需要记住字符a的ASCII，只要求出一个相对数值就可以了
+            record[ord(i) - ord("a")] += 1
+        for i in t:
+            record[ord(i) - ord("a")] -= 1
+        for i in range(26):
+            if record[i] != 0:
+                #record数组如果有的元素不为零0，说明字符串s和t 一定是谁多了字符或者谁少了字符。
+                return False
+        return True
+    
+
+
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        from collections import  defaultdict
+        
+        s_dict = defaultdict(int)
+        t_dict = defaultdict(int)
+        for x in s:
+            s_dict[x] += 1
+        
+        for x in t:
+            t_dict[x] += 1
+        return s_dict == t_dict
+    
+
+class Solution(object):
+    def isAnagram(self, s: str, t: str) -> bool:
+        from collections import Counter
+        a_count = Counter(s)
+        b_count = Counter(t)
+        return a_count == b_count
+
+
+#349两个数组的交集
+'''
+题意：给定两个数组，编写一个函数来计算它们的交集。
+
+说明： 输出结果中的每个元素一定是唯一的。 我们可以不考虑输出结果的顺序
+'''
+#使用字典和集合
+
+class Solution:
+    def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
+    # 使用哈希表存储一个数组中的所有元素
+        table = {}
+        for num in nums1:
+            table[num] = table.get(num, 0) + 1
+        
+        # 使用集合存储结果
+        res = set()
+        for num in nums2:
+            if num in table:
+                res.add(num)
+                del table[num]
+        
+        return list(res)
+
+
+#使用数组
+	
+class Solution:
+    def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        count1 = [0]*1001
+        count2 = [0]*1001
+        result = []
+        for i in range(len(nums1)):
+            count1[nums1[i]]+=1
+        for j in range(len(nums2)):
+            count2[nums2[j]]+=1
+        for k in range(1001):
+            if count1[k]*count2[k]>0:
+                result.append(k)
+        return result
+
+
+#使用集合
+class Solution:
+    def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        set1 = set(nums1)
+        set2 = set(nums2)
+        return list(set1 & set2)
